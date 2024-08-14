3eb43bb405d84255339b99db61048eda3a9991c7
Haroon <you@example.com>
2024-08-14 16:49:59 +0300

    Refactor App component to use hooks and add track management functionality

--- src/services/trackService.js
+++ src/services/trackService.js
@@ -1,8 +1,6 @@
 
 const BASE_URL = `${import.meta.env.VITE_BACK_END_SERVER_URL}/tracks`;
 
-// const cors = require('cors');
-
 
 const index = async () => {
     try {
@@ -16,15 +14,10 @@ const index = async () => {
   const create = async (formData) => {
     try {
       const res = await fetch(BASE_URL, {
-        // We specify that this is a 'POST' request
         method: 'POST',
-        // We're sending JSON data, so we attach a Content-Type header
-        // and specify that the data being sent is type 'application/json'
         headers: {
           'Content-Type': 'application/json',
         },
-        // The formData, converted to JSON, is sent as the body
-        // This will be received on the backend as req.body
         body: JSON.stringify(formData),
       });
       return res.json();
